23cbeeb7fe1185e1913a6b092833d82638f9693a
chiadikaobixo <chiadikaobixo@gmail.com>
2022-07-19 08:30:16 -0500

    renaming and deleting of fileVersion

--- fileVersion/service.ts
+++ fileVersion/service.ts
@@ -67,10 +67,30 @@ export async function getFileVersions(
   return await client.fileVersion.findMany({
     ...(pagination
       ? {
-          skip: (pagination.page -1) * pagination.pageLength,
+          skip: (pagination.page - 1) * pagination.pageLength,
           take: pagination.pageLength,
         }
       : {}),
     where: { fileId },
   })
+}
+
+export async function renameFileVersion(
+  client: PrismaClient,
+  id: FileVersion["id"],
+  name: FileVersion["name"]
+): Promise<FileVersion> {
+  return await client.fileVersion.update({
+    where: { id },
+    data: { name },
+  })
+}
+
+export async function deleteFileVersion(
+  client: PrismaClient,
+  id: FileVersion["id"]
+): Promise<boolean> {
+  const version = await client.fileVersion.delete({ where: { id } })
+  await getBucket().deleteObject(version.key)
+  return true
 }
