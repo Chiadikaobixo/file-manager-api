a58053586b54641a0c4079648370c883f0a84ae2
chiadikaobixo <chiadikaobixo@gmail.com>
2022-07-25 08:54:14 -0500

    soft delete middleware

--- prisma/config.ts
+++ prisma/config.ts
@@ -1,8 +1,32 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-export function prismaClient(): PrismaClient{
-    const prisma = new PrismaClient({
-        log: ["query", "info", "warn", "error"]
-    })
-    return prisma
+export function prismaClient(): PrismaClient {
+  const prisma = new PrismaClient({
+    log: ["query", "info", "warn", "error"],
+  })
+
+  prisma.$use(async (params, next) => {
+    const types = ["File", "FileVersion", "Directory"]
+    if (types.includes(params.model ?? "")) {
+      if (params.action === "delete") {
+        params.action = "update"
+        if (params.args.data !== undefined) {
+          params.args.data["deletedAt"] = new Date()
+        } else {
+          params.args["data"] = { deletedAt: new Date() }
+        }
+      }
+      if (params.action === "deleteMany") {
+        params.action = "updateMany"
+        if (params.args.data !== undefined) {
+          params.args.data["deletedAt"] = new Date()
+        } else {
+          params.args["data"] = { deletedAt: new Date() }
+        }
+      }
+    }
+    return next(params)
+  })
+
+  return prisma
 }
